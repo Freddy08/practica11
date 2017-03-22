2ff3705a8fd7a253d9a690b750b309de3f6d9345
Freddy Serrano Ochoa <ferrariunach@outlook.es>
2017-03-21 19:59:14 -0600

    practica11
    Tarea en laboratorio

---
+++ Practica11/index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>Practica 11</title>
+    </head>
+    <body>
+        <?php
+        //Nombre del alumno freddy serrano ochoa
+        //Usando variables de php
+        echo "<h1>Variables</h1>";
+        $nombre='Freddy';
+        $apellidos='Serrano Ochoa';
+        $edad=21;
+        $salario= 777345.12;
+        $fecha_nacimiento='08/11/1995';
+        echo 'Nombre  completo '.$nombre.'</br>';
+        echo $apellidos.'</br>';
+        echo $edad.'</br>';
+        echo $salario.'</br>';
+        echo $fecha_nacimiento.'</br>';
+        
+       
+        ?>
+        <p>Dime tu nombre:</p>
+        <input type="text" value =""
+        //<?php 
+        //    echo $row['nombre']; 
+        //?> >
+    </body>
+</html>
